51527089fb7c03053e955339e822d902d4ec7fee
Jason Law <jasonlaw89@gmail.com>
2014-10-22 12:22:07 +0800

    add spec for image uploader

--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -5,4 +5,18 @@ feature 'visit_homepage' do   #use this keyword to define the feature"
     visit '/'
     expect(page).to have_content 'Hello World!'
   end
+
+  scenario 'Privacy' do
+    visit root_path
+    click_link 'Privacy'
+    expect(page).to have_content 'Privacy'
+  end
+
+  scenario 'Terms and Conditions' do
+    visit root_path
+    click_link 'Terms and Conditions'
+    # expect(page).have_selector('h1'))
+    # except(page).to have_content 'Terms'
+    expect(page).to have_xpath('//h1[text() = "Terms and Conditions"]')
+  end
 end
